7dba458151cf530b51a70d2fcf751367cc2d7093
rewanth1997 <rewanth1997@gmail.com>
2016-10-12 12:25:48 +0000

    moved router.details from auth.js to index.js

--- routes/auth.js
+++ routes/auth.js
@@ -67,35 +67,4 @@ router.get('/signout',function(req, res, next) {
    }
 });
 
-router.get('/details',function(req,res,next){
-  console.log("Error code = " + res.status(302).render('login'));
-});
-
-router.post('/details',function(req, res, next){
-  var username = req.body.username;
-  var rollNo = req.body.rollNo;
-  var phoneNo = req.body.phoneNo;
-  var interests = req.body.interests;
-  var stringOfInterests = "";
-  for(var i=0; i<interests.length; i++){
-    stringOfInterests += interests[i] + ',';
-  }
-  console.log(username + " " + rollNo + " " + phoneNo + " " + stringOfInterests);
-  
-  var detailsPromise = null;
-  detailsPromise = new Model.Details({username: username}).fetch();
-
-  detailsPromise.then(function(model) {
-      if(model) {
-         res.render('redirection',{title: 'Success', user: username});
-      } else {
-         var fillUserDetails = new Model.Details({username: username, rollNo: rollNo, phoneNo: phoneNo, interests: stringOfInterests});
-         fillUserDetails.save().then(function(model) {
-            res.render('redirection',{title: 'Success', user: username});
-         });
-      }
-   });
-
-});
-
 module.exports = router;
