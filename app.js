77386a3da74499d070da8a91879fb6d37bfdda64
abhishekg785 <abhishekg785@gmail.com>
2016-10-12 19:11:56 +0530

    login post route changed

--- app.js
+++ app.js
@@ -68,21 +68,6 @@ app.use(function(err, req, res, next) {
   });
 });
 */
-passport.use(new LocalStrategy(function(username, password, done) {
-  new Model.User({username: username}).fetch().then(function(data) {
-    var user = data;
-    if(user === null) {
-      return done(null, false, {message: 'Invalid username or password'});
-    } else {
-        user = data.toJSON();
-        if(!bcrypt.compareSync(password, user.password)) {
-          return done(null, false, {message: 'Invalid username or password'});
-        } else {
-            return done(null, user);
-          }
-      }
-  });
-}));
 
 passport.serializeUser(function(user, done) {
   done(null, user.username);
