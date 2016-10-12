03e22465ea6a53d9f3237e35a22e78197aec8d9f
abhishekg785 <abhishekg785@gmail.com>
2016-10-13 03:51:18 +0530

    indentaion of 4 spaces added to db.js

--- database/db.js
+++ database/db.js
@@ -1,15 +1,15 @@
 var config = require('../config');
 
 var knex = require('knex')({
-        client: 'mysql',
-        connection: {
-            host: config.host,
-            user: config.user,
-            password: config.password,
-            database: config.database,
-            charset  : config.charset
-        }
-    });
+    client: 'mysql',
+    connection: {
+        host: config.host,
+        user: config.user,
+        password: config.password,
+        database: config.database,
+        charset  : config.charset
+    }
+});
 
 var Bookshelf = require('bookshelf')(knex);
 
