bd8571e93b225623f58133fc71903996220ef278
Anshul <malikanshul29@gmail.com>
2016-11-04 01:30:11 +0530

    From scratch after schema redesign

--- app.js
+++ app.js
@@ -12,16 +12,8 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 var index = require('./routes/index');
-var auth = require('./routes/auth');
-var events = require('./routes/events');
-var Model = require('./models/model');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html',require('ejs').renderFile);
-app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -31,12 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret: 'secret code'}));
-app.use(passport.initialize());
-app.use(passport.session());
 
-app.use('/',index);
-app.use('/auth',auth);
-app.use('/api/events',events);
+app.use('/', index);
 
 /*
 //catch 404 and forward to error handler
@@ -70,31 +58,4 @@ app.use(function(err, req, res, next) {
   });
 });
 */
-passport.use(new LocalStrategy(function(username, password, done) {
-  new Model.User({username: username}).fetch().then(function(data) {
-    var user = data;
-    if(user === null) {
-      return done(null, false, {message: 'Invalid username or password'});
-    } 
-    else {
-        user = data.toJSON();
-        if(!bcrypt.compareSync(password, user.password)) {
-          return done(null, false, {message: 'Invalid username or password'});
-        } else {
-            return done(null, user);  //call the serialize
-          }
-    }
-  });
-}));
-
-passport.serializeUser(function(user, done) {
-  done(null, user.username);
-});
-
-passport.deserializeUser(function(username, done) {
-  new Model.User({username: username}).fetch().then(function(user) {
-    done(null, user);
-  });
-});
-
 module.exports = app;
